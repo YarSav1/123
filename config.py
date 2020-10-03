b746ff9558c4b638a57b71fd31687976be9635eb
YarSav1 <57031061+YarSav1@users.noreply.github.com>
2020-10-03 15:26:37 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 settings = {
-    'TOKEN': 'NzM3MjYwODIyNjY0OTcwMjYx.Xx6xpQ.MxDIJbMTFPL4qScZ68Vwt7zopco',
-    'NAME': 'YS',
-    'ID': 737260822664970261,
+    'TOKEN': 
+    'NAME': '
+    'ID': 0
     'PREFIX': '!'
 }
